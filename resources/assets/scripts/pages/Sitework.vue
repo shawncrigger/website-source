f89ac26b8fa14206c29915c1b9f6f0c639cbaf82
Shawn Crigger <ithippyshawn@gmail.com>
2018-09-14 17:44:12 -0400

    Update sitework view to use thumbs

--- resources/assets/scripts/pages/Sitework.vue
+++ resources/assets/scripts/pages/Sitework.vue
@@ -5,16 +5,18 @@
 			<div class="col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 				<h2 class="title">Job Descripition</h2>
 				<p class="text-muted">
-				This project is based on Wordpress using a Custom Theme I wrote, along with a few custom plug-ins and widgets that were created for the project.   The Front Page is widget based which allows the end client to pick what type of content to display on each row and add and remove rows very easily,  there are a couple custom page types and taxonimies for there specials and team members.  Along with a event calendar that required quite a bit of modification. to fit there design.  I built this website without using a responsive framework, and in hindsite I wish I would have used Foundation or Bootstrap or something.
+				{{website.full_description}}
 				</p>
 			</div>
-			<div class="col-md-6 col-lg-6 col-xl-4 col-xxl-6" v-for="item in images">
+			<div class="col-md-6 col-lg-6 col-xl-4 col-xxl-6" v-for="item in thumbs">
 				<div class="portfolio__item" :style="{'background-image' : 'url('+item+')'}">
 				</div>
 			</div>
 		</div>
 	</div>
 </template>
+
+
 <script>
 export default {
 	    data:function(){
@@ -31,6 +33,12 @@ export default {
 	        console.log(json);
 	    },
 	    computed: {
+	    	thumbs(){
+	    		return this.website.images.thumbs;
+	    	},
+	    	fullsize(){
+	    		return this.website.images.full;
+	    	},
 	    	images(){
 	    		return this.website.images;
 	    	}
