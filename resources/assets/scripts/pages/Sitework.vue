1dd23e3e1b6af2bb164d1410019fe55423e18c54
Shawn Crigger <ithippyshawn@gmail.com>
2018-09-15 03:16:19 -0400

    Change view to use new image array format

--- resources/assets/scripts/pages/Sitework.vue
+++ resources/assets/scripts/pages/Sitework.vue
@@ -8,8 +8,8 @@
 				{{website.full_description}}
 				</p>
 			</div>
-			<div class="col-md-6 col-lg-6 col-xl-4 col-xxl-6" v-for="item in images">
-				<div class="portfolio__item" :style="{{'background-image' : 'url('+item.thumb+')'}}">
+			<div class="col-md-6 col-lg-6 col-xl-4 col-xxl-6" v-for="item in website.images.thumbs">
+				<div class="portfolio__item" :style="{{'background-image' : 'url('+item+')'}}">
 				</div>
 			</div>
 		</div>
