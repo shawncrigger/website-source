88a837e19b25e76c94194ddc06b407c09145a282
Shawn Crigger <ithippyshawn@gmail.com>
2018-09-15 14:55:14 -0400

    Add photogallery to portfolio pages

--- resources/assets/scripts/pages/Sitework.vue
+++ resources/assets/scripts/pages/Sitework.vue
@@ -3,25 +3,33 @@
 		<h1 class="title">{{website.title}}</h1>
 		<div class="row no-gutters portfolio">
 			<div class="col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-				<h2 class="">Job Descripition</h2>
+				<h2 class="">Job Description</h2>
 				<p class="text-muted">
 				{{website.full_description}}
 				</p>
 			</div>
-			<div class="col-md-6 col-lg-6 col-xl-4 col-xxl-6" v-for="(item, index) in thumbs">
-				<div class="portfolio__item" :style="{{'background-image' : 'url('+item[index]+')'}}">
-				</div>
-			</div>
+			<div class="portfolio-gallery">
+			   <gallery :images="fullsize" :index="index" @close="index = null"></gallery>
+			   <div
+			     class="image"
+			     v-for="(image, imageIndex) in fullsize"
+			     :key="imageIndex"
+			     @click="index = imageIndex"
+			     :style="{ backgroundImage: 'url(' + image + ')', width: '300px', height: '200px' }"
+			   ></div>
+			 </div>
 		</div>
 	</div>
 </template>
 
 
 <script>
+import VueGallery from 'vue-gallery';
 export default {
 	    data:function(){
 	      return {
-	        website:[]
+	        website:[],
+	        index: null
 	      };
 	    },
 	    created: function () {
@@ -42,6 +50,9 @@ export default {
 	    	images(){
 	    		return this.website.images;
 	    	}
+	    },
+	    components: {
+	      'gallery': VueGallery
 	    }
 	}
 </script>
