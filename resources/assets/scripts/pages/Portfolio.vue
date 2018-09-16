5157c90004e37eade8b7e968e2d2be5655c2e819
Shawn Crigger <ithippyshawn@gmail.com>
2018-09-16 15:08:26 -0400

    Update portfolio page text

--- resources/assets/scripts/pages/Portfolio.vue
+++ resources/assets/scripts/pages/Portfolio.vue
@@ -1,6 +1,9 @@
 <template>
 	<div>
 		<h1 class="title">Portfolio</h1>
+		<p class="text-muted">
+		These are some of the websites , I have built over the years that I am particularly proud of.   If I included every website I touched over the last decade on this page it would scroll for miles, so these are  just my favorites so to speak.
+		</p>
 		<div class="row no-gutters portfolio">
 			<div class="col-md-6 col-lg-6 col-xl-4 col-xxl-6" v-for="item in items">
 				<div class="portfolio__item" :style="{'background-image' : 'url('+item.thumbnail+')'}">
