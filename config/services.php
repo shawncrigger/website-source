7ba9e892f29f50359088400b67f908d8fcacea7b
Shawn Crigger <ithippyshawn@gmail.com>
2018-09-19 08:35:56 -0400

    forgot to add to services config file

--- config/services.php
+++ config/services.php
@@ -4,4 +4,8 @@ return [
 	'sparkpost' => [
 	    'secret' => 'eccf6a83a462ec7ddc6fef58a833c5450c10f68b',
 	],
+	'mailgun' => [
+	    'domain' => env('MAILGUN_DOMAIN'),
+	    'secret' => env('MAILGUN_API_KEY'),
+	],
 ];
