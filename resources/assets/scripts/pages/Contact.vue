ccfe8327b4cd042a7449279017cdaca25bdbe4d4
Shawn Crigger <42555674+shawncrigger@users.noreply.github.com>
2021-02-21 12:37:58 -0500

    Updated Contact Page
    - Fixed Linked In Link
    - Changed some wording around
    - Added newer github

--- resources/assets/scripts/pages/Contact.vue
+++ resources/assets/scripts/pages/Contact.vue
@@ -1,76 +1,74 @@
 <template>
-	<div>
-		<div class="row">
-			<div class="col-lg-6 col-primary">
-				<contact-form></contact-form>
-				<map-canvas position="33.674700,-78.901180" zoom="4" icon="/images/mapmarker.png"></map-canvas>
-			</div>
-			<div class="col-lg-6 flex-first flex-lg-unordered">
-				<h1 class="title title--spaced">Contact me</h1>
-				<div class="list-group">
-					<div class="list-group-item">
-						<h2 class="title">I'm Social</h2>
-			          <ul class="social-icons">
-			            <li>
-			              <a href="https://twitter.com/shawncrigger" class="twitter" target="_blank">
-			                <span><i class="fa fa-tumblr fa-stack-1x fa-inverse"></i></span>
-			                <span>https://twitter.com/shawncrigger</span>
-			              </a>
-			            </li>
-			            <li>
-			              <a href="https://twitter.com/shawncrigger" class="linkedin" target="_blank">
-			                <span><i class="fa fa-linkedin fa-stack-1x fa-inverse"></i></span>
-			                <span>https://www.linkedin.com/in/shawn-crigger/</span>
-			              </a>
-			            </li>
-			            <li>
-			              <a href="https://github.com/svizion?tab=repositories" class="github" target="_blank">
-			                <span><i class="fa fa-github fa-stack-1x fa-inverse"></i></span>
-			                <span>https://github.com/svizion?tab=repositories</span>
-			              </a>
-			            </li>
-			            <li>
-			              <a href="https://www.upwork.com/o/profiles/users/_~012c863ad135fdb85f/" class="upwork" target="_blank">
-			                <span><i class="upwork"></i></span>
-			                <span>https://www.upwork.com/o/profiles/users/_~012c863ad135fdb85f/k</span>
-			              </a>
-			            </li>
-<!--
-			            <li>
-			            	<a href="#" href="javascript:window.location.href = 'mailto:' + ['ithippyshawn','gmail.com'].join('@')" class="email" target="_blank">
-			                <span><i class="fa fa-envelope-open-o fa-stack-1x fa-inverse"></i></span>
-			                <span>ithippyshawn@gmail.com</span>
-			              </a>
-			            </li>
--->
-			          </ul>
-					</div>
-					<div class="list-group-item">
-						<h2 class="title">My Goals in Life</h2>
-						<p class="text-muted">As developer nomad, over the last few years my wife and I considered our RV our home and where ever we stopped for the night or week was home for the night. That was fun for awhile, but now I want to buy some land and give my dog the backyard she deserves. Currently we live in Myrtle Beach, SC but have been thinking of relocating.</p>
-					</div>
-					<div class="list-group-item">
-						<h3 class="title">The Team<br></h3>
-						<p class="text-muted">My team consists of my main supervisor, Mittens she's a 4 year old Hound Mix, who likes to stare over my shoulder to make sure I'm doing things how she would do them.  Next up is my wife Mellissa Crigger who is mostly my proofreader. Proof I can read (joke) and she helps with invoicing and customer service.</p>
-					</div>
-					<div class="list-group-item">
-						<h3 class="title">Mittens<br></h3>
-						<iframe id="myplayer" src="//player.vimeo.com/video/289607133?player_id=myplayer&api=1" width="640" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-					</div>
-				</div>
-			</div>
-		</div>
+    <div>
+        <div class="row">
+            <div class="col-lg-6 col-primary">
+                <contact-form></contact-form>
+                <map-canvas position="33.674700,-78.901180" zoom="4" icon="/images/mapmarker.png"></map-canvas>
+            </div>
+            <div class="col-lg-6 flex-first flex-lg-unordered">
+                <h1 class="title title--spaced">Contact me</h1>
+                <div class="list-group">
+                    <div class="list-group-item">
+                        <h2 class="title">I'm Social</h2>
+                      <ul class="social-icons">
+                        <li>
+                          <a href="https://twitter.com/shawncrigger" class="twitter" target="_blank">
+                            <span><i class="fa fa-tumblr fa-stack-1x fa-inverse"></i></span>
+                            <span>Twitter</span>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="https://www.linkedin.com/in/shawn-crigger/" class="linkedin" target="_blank">
+                            <span><i class="fa fa-linkedin fa-stack-1x fa-inverse"></i></span>
+                            <span>LinkedIn</span>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="https://github.com/shawncrigger?tab=repositories" class="github" target="_blank">
+                            <span><i class="fa fa-github fa-stack-1x fa-inverse"></i></span>
+                            <span>Newer Github</span>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="https://github.com/svizion?tab=repositories" class="github" target="_blank">
+                            <span><i class="fa fa-github fa-stack-1x fa-inverse"></i></span>
+                            <span>Older Github Account</span>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="https://www.upwork.com/o/profiles/users/_~012c863ad135fdb85f/" class="upwork" target="_blank">
+                            <span><i class="upwork"></i></span>
+                            <span>Upwork</span>
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                    <div class="list-group-item">
+                        <h2 class="title">My Goals in Life</h2>
+                        <p class="text-muted">I used to travel alot and develop on the road, Now I live in a nice townhouse with a home office, right outside Myrtle Beach, SC but have been thinking of relocating.</p>
+                    </div>
+                    <div class="list-group-item">
+                        <h3 class="title">The Team<br></h3>
+                        <p class="text-muted">My team consists of my main supervisor, Mittens she's a 5 year old Hound Mix, who likes to stare over my shoulder to make sure I'm doing things how she would do them. Dogs are just amazing how much happiness they bring to life.</p>
+                    </div>
+                    <div class="list-group-item">
+                        <h3 class="title">Mittens<br></h3>
+                        <iframe id="myplayer" src="//player.vimeo.com/video/289607133?player_id=myplayer&api=1" width="640" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-	</div>
+    </div>
 </template>
 <script>
 import MapCanvas from '../components/MapCanvas.vue';
 import ContactForm from '../components/ContactForm.vue';
 import Typed from 'typed.js'
 export default {
-	components : {
-		MapCanvas,
-		ContactForm
-	}
+    components : {
+        MapCanvas,
+        ContactForm
+    }
 }
 </script>
