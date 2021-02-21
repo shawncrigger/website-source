af97de47b706ab229b6b66dd9b46d257954ac859
Shawn Crigger <42555674+shawncrigger@users.noreply.github.com>
2021-02-21 12:08:47 -0500

    Updated the uses page
    - Fixed some grammar errors
    - Replaced mobile office with home office and added some new tools

--- resources/assets/scripts/pages/Uses.vue
+++ resources/assets/scripts/pages/Uses.vue
@@ -17,7 +17,7 @@
                <li><a rel="nofollow" target="_blank" href="https://www.freshworks.com/project-management/">Freshworks</a> on my main project to keep track of bugs and general project management.</li>
                <li>I use <a href="https://macdown.uranusjr.com/" rel="nofollow">MacDown</a> to write must of my MarkDown and a lot of times I use it to write the basic HTML, every page on this website was written using MacDown.</li>
                <li>I use Grammarly to write ideas and also to check my texts to avoid errors and find the perfect words to express me when I need to write in English.</li>
-               <li>I use <a rel="nofollow" target="_blank" href="https://insomnia.rest/">insomnia REST</a> client to test APIs requests.</li>
+               <li>I use <a rel="nofollow" target="_blank" href="https://insomnia.rest/">Insomnia REST</a> client to test APIs requests.</li>
             </ul>
             <h2>🔦 Developer Services</h2>
             <ul>
@@ -25,17 +25,18 @@
                <li>At work, I develop with <a rel="nofollow" target="_blank" href="https://aws.amazon.com/">Amazon Web Services</a>, Load Balancer, CloudFront CDN, User System, everything and I have to say it&#39;s awesome.</li>
                <li>I also use <a rel="nofollow" target="_blank" href="https://wpengine.com/">WP Engine</a> to host a lot of our Wordpress clients, I love how they give a CDN with there plans.</li>
                <li><a rel="nofollow" target="_blank" href="https://letsencrypt.org/">Let&#39;s Encrypt</a> has changed the game with SSL certs, I remember back when it cost $200+ just for the cert.</li>
-               <li>I also build a lot of single website&#39;s there own <a rel="nofollow" target="_blank" href="https://m.do.co/c/860cf3a53cab">Digital Ocean</a> server.</li>
+               <li>I also build a lot of single website&#39;s thier own <a rel="nofollow" target="_blank" href="https://m.do.co/c/860cf3a53cab">Digital Ocean</a> server.</li>
                <li>For testing responsive emails, I use either <a rel="nofollow" target="_blank" href="https://litmus.com/">Litmus</a> and <a rel="nofollow" target="_blank" href="https://www.emailonacid.com/">Email on ACID</a>.</li>
                <li>And finally for responsive website&#39;s <a rel="nofollow" target="_blank" href="https://www.browserstack.com/">BrowserStack</a></li>
+               <li>Also for responsive website&#39;s I have been using a new browser <a rel="nofollow" target="_blank" href="https://sizzy.co/">Sizzy</a></li>
+               <li>Another great tool I have found and love is <a rel="nofollow" target="_blank" href="https://www.notion.so/">Notion</a> which is great for project planning, task tracking, notebooking, and it imports from everything popular. Very neat software</li>
                <li>Taking screenshots of the website&#39;s <a rel="nofollow" target="_blank" href="https://browshot.com/">Browshot</a> is a great service that I have been using a lot lately.</li>
             </ul>
-            <h2>💻 Mobile Office Setup</h2>
+            <h2>🏡 Home Office Setup</h2>
             <ul>
                <li>My current machine is a 2018 Macbook Pro 2.2 GHz 6-Core Intel Core i7 with 16GB ram and Intel UHD Graphics 630 1536 MB graphics card.</li>
-               <li>For Internet connection, I use an unlocked Verizon MiFi 6620L Jetpack Mobile Hotspot with an unlimited plan from Verizon.</li>
-               <li>I carry everything in a Thule backpack which is comfortable, and it fits my Laptop, paper notebook, a couple of pens, flash drives, spare earbuds, etc.</li>
-               <li>Since I move around a lot I travel light, if it doesn&#39;t fit in my backpack it isn&#39;t needed.</li>
+               <li>For Internet connection, I have cable from Spectrum, I also have a unlocked Verizon MiFi 6620L Jetpack Mobile Hotspot incase I need to travel.</li>
+               <li>A Desk with Multiple Monitors, and my favorite upgrade was adding roller blade wheels to my computer chair.</li>
             </ul>
          </section>
       </div>
